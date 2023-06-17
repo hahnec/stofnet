df8c3101470b858ecac7febc58ba4667c78aec46
hahnec <christopher.hahne@unibe.ch>
2023-06-17 21:18:29 +0200

    fix(dev): convert samples to cpu and numpy

--- utils/mask2samples.py
+++ utils/mask2samples.py
@@ -26,7 +26,7 @@ def samples2nested_list(scores, window_size):
     for bidx in range(max(indices[:, 0])+1):
         for cidx in range(max(indices[:, 1])+1):
             samples = indices[(indices[:, 0]==bidx) & (indices[:, 1]==cidx), -1]
-            helper_list.append(samples)
+            helper_list.append(samples.cpu().numpy())
         nested_list.append(helper_list)
         helper_list = []
 
