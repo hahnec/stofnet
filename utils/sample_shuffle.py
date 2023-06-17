336b6f07588b56cfa7806253ce6255a15f4e0e68
hahnec <christopher.hahne@unibe.ch>
2023-06-18 00:18:36 +0200

    fix(shuffle): use alternative 1D shuffling

--- utils/sample_shuffle.py
+++ utils/sample_shuffle.py
@@ -8,12 +8,22 @@ class SampleShuffle1D(nn.Module):
         self.upsample_factor = upsample_factor
 
     def forward(self, x):
-        batch_size, channels, sequence_length = x.size()
-        assert channels % self.upsample_factor == 0, "Input channels must be divisible by upsample_factor."
+
+        #batch_size, channels, sequence_length = x.size()
+        #assert channels % self.upsample_factor == 0, "Input channels must be divisible by upsample_factor."
+        #unfolded = x.unfold(2, self.upsample_factor, self.upsample_factor).permute(0, 2, 1, 3)
+        #unfolded = unfolded.contiguous().view(batch_size, sequence_length * self.upsample_factor, channels // self.upsample_factor)
+        #x = unfolded.permute(0, 2, 1)
         
-        unfolded = x.unfold(2, self.upsample_factor, self.upsample_factor).permute(0, 2, 1, 3)
-        unfolded = unfolded.contiguous().view(batch_size, sequence_length * self.upsample_factor, channels // self.upsample_factor)
-        output = unfolded.permute(0, 2, 1)
-        
-        return output
+        # https://github.com/serkansulun/pytorch-pixelshuffle1d/blob/master/pixelshuffle1d.py
+        batch_size, short_channel_len, short_width = x.shape
+
+        long_channel_len = short_channel_len // self.upsample_factor
+        long_width = self.upsample_factor * short_width
+
+        x = x.contiguous().view([batch_size, self.upsample_factor, long_channel_len, short_width])
+        x = x.permute(0, 2, 3, 1).contiguous()
+        x = x.view(batch_size, long_channel_len, long_width)
+
+        return x
     
