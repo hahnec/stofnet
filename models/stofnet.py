f431e9dc4ccb125eeea95790702350e40f166275
hahnec <christopher.hahne@unibe.ch>
2023-06-21 18:37:31 +0200

    refactor(net): shrink model and use comments

--- models/stofnet.py
+++ models/stofnet.py
@@ -11,74 +11,55 @@ class StofNet(nn.Module):
 
     def __init__(self, upsample_factor, hilbert_opt=True, concat_oscil=True):
         super(StofNet, self).__init__()
-
+    
+        # input signal handling
         self.hilbert = HilbertTransform(concat_oscil=concat_oscil) if hilbert_opt else None
         in_channels = 2 if hilbert_opt and concat_oscil else 1
 
+        # init first and last layer
         self.conv1 = nn.Conv1d(in_channels, 64, 9, 1, 4)
-        self.conv2 = nn.Conv1d(64, 64, 3, 1, 1)
-        self.conv3 = nn.Conv1d(64, 64, 3, 1, 1)
-        self.conv4 = nn.Conv1d(64, 64, 3, 1, 1)
-        self.conv5 = nn.Conv1d(64, 64, 3, 1, 1)
-        self.conv6 = nn.Conv1d(64, 64, 3, 1, 1)
-        self.conv7 = nn.Conv1d(64, 64, 3, 1, 1)
-        self.conv8 = nn.Conv1d(64, 64, 3, 1, 1)
-        self.conv9 = nn.Conv1d(64, 64, 3, 1, 1)
-        self.conv10 = nn.Conv1d(64, 64, 3, 1, 1)
-        self.conv11 = nn.Conv1d(64, 64, 3, 1, 1)
-        self.conv12 = nn.Conv1d(64, 64, 3, 1, 1)
-
         self.conv13 = nn.Conv1d(64, upsample_factor, 3, 1, 1)
-        
+
+        # init remaining layers
+        for i in range(2, 13):
+            setattr(self, f'conv{i}', nn.Conv1d(64, 64, 3, 1, 1))
+
+        # shuffle feature channels to high resolution output
         self.sample_shuffle = SampleShuffle1D(upsample_factor)
-        
+
+        # indices of layers where residual connections are added and ReLU is not used
+        self.residual_layers = [3, 5, 7, 9, 11, 12, 13]
+
     def forward(self, x):
-
-        x = self.hilbert(x) if self.hilbert is not None else x
-
-        x = F.relu(self.conv1(x))
-        res1=x
-        x = F.relu(self.conv2(x))
-        x = self.conv3(x)
-        x=torch.add(res1,x)
-        res3=x
-        x = F.relu(self.conv4(x))
-        x = self.conv5(x)
-        x=torch.add(res3,x)
-        res5=x
-        x = F.relu(self.conv6(x))
-        x = self.conv7(x)
-        x=torch.add(res5,x)
-        res7=x
-        x = F.relu(self.conv8(x))
-        x = self.conv9(x)
-        x=torch.add(res7,x)
-        res9=x
-        x = F.relu(self.conv10(x))
-        x = self.conv11(x)
-        x=torch.add(res9,x)
-        x = self.conv12(x)
-        x=torch.add(res1,x)
         
+        # input signal handling
+        x = self.hilbert(x) if self.hilbert is not None else x
+        
+        # first layer
+        x = F.relu(self.conv1(x))
+        res, res1 = x, x
+
+        # iterate through convolutional layers
+        for i in range(2, 12):
+            # get corresponding convolutional layer
+            conv = getattr(self, f'conv{i}')
+            # pass data to layer considering residual connection
+            x = torch.add(res, conv(x)) if i in self.residual_layers else F.relu(conv(x))
+            # store residuals
+            if i in self.residual_layers: res = x
+
+        # second last layer
+        x = torch.add(res1, self.conv12(x))
+
+        # last layer and shuffling
         x = self.sample_shuffle(self.conv13(x))
         
         return x
 
     def _initialize_weights(self):
-        init.orthogonal(self.conv1.weight, init.calculate_gain('relu'))
-        init.orthogonal(self.conv2.weight, init.calculate_gain('relu'))
-        init.orthogonal(self.conv3.weight)
-        init.orthogonal(self.conv4.weight, init.calculate_gain('relu'))
-        init.orthogonal(self.conv5.weight)
-        init.orthogonal(self.conv6.weight, init.calculate_gain('relu'))
-        init.orthogonal(self.conv7.weight)
-        init.orthogonal(self.conv8.weight, init.calculate_gain('relu'))
-        init.orthogonal(self.conv9.weight)
-        init.orthogonal(self.conv10.weight, init.calculate_gain('relu'))
-        init.orthogonal(self.conv11.weight)
-        init.orthogonal(self.conv12.weight)
-        init.orthogonal(self.conv13.weight)
-        
-        
-        
-        
+
+        for i in range(1, 14):
+            if i not in self.residual_layers:
+                init.orthogonal(getattr(self, f'conv{i}').weight, init.calculate_gain('relu'))
+            else:
+                init.orthogonal(getattr(self, f'conv{i}').weight)
