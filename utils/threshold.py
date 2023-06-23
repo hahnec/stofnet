7cfc750a4b97f18733991ed799cbfaa6dc8ebba0
hahnec <christopher.hahne@unibe.ch>
2023-06-23 10:19:15 +0200

    fix(odd): ensure odd window size

--- utils/threshold.py
+++ utils/threshold.py
@@ -6,6 +6,7 @@ from sklearn.metrics import roc_curve
 def find_threshold(masks_pred, masks_true, window_size=None):
 
     if window_size:
+        window_size = window_size//2*2+1
         max_scores = torch.nn.functional.max_pool1d(masks_pred, window_size, stride=1, padding=(window_size - 1) // 2).squeeze(0)
         suppressed = (masks_pred == max_scores).float() * masks_pred
     else:
