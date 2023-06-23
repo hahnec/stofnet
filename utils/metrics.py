9e218caf335c3a9d692713e0f41670610c368627
hahnec <christopher.hahne@unibe.ch>
2023-06-23 12:40:07 +0200

    fix(valid): extract valid samples prior to analysis

--- utils/metrics.py
+++ utils/metrics.py
@@ -3,22 +3,35 @@ import numpy as np
 
 
 jaccard_index = lambda tp, fn, fp: tp/(fn+tp+fp)*100
+remove_invalids = lambda x: x[(x!=0) & (~torch.isnan(x)) & (~torch.isinf(x))]
 
 
 def toa_rmse(gt_samples, es_samples, tol=1):
 
-    gt_samples[gt_samples==0] = torch.nan
-    distances = abs(gt_samples[..., None] - es_samples[..., None, :].repeat(1, gt_samples.shape[-1], 1))
-    mins, args = distances.min(-1)
+    # initialize output variables
+    batch_dim = gt_samples.shape[0]
+    mes, tps, fps, fns = [torch.zeros(batch_dim, device=gt_samples.device) for _ in range(4)]
 
-    tps = (mins <=tol).sum(-1)
-    fns = (mins > tol).sum(-1)
-    fps = (es_samples > 0).sum(-1) - tps
+    # iterate through batch dimension (samples are of unequal size per batch)
+    for batch_idx in range(batch_dim):
+
+        # remove Zeros and NaNs and Infs
+        gt_valid = remove_invalids(gt_samples[batch_idx])
+        es_valid = remove_invalids(es_samples[batch_idx])
+
+        # replicate estimates to get distances
+        distances = abs(gt_valid[:, None] - es_valid[None, :].repeat(gt_valid.shape[-1], 1))
+        mins, args = distances.min(-1)
+
+        # compute metrics
+        mes[batch_idx] = torch.mean(mins[mins<=tol], dim=-1)
+        tps[batch_idx] = (mins <=tol).sum(-1).float()
+        fns[batch_idx] = (mins > tol).sum(-1).float()
+        fps[batch_idx] = es_valid.shape[-1] - tps[batch_idx]
+
+    # compute metrics (vectorized)
     jaccards = jaccard_index(tps, fns, fps)
     precisions = tps/(fps+tps) * 100
     recalls = tps/(fns+tps) * 100
 
-    mins[mins>tol] = torch.nan
-    mes = torch.nanmean(mins, dim=-1)
-
-    return mes, precisions, recalls, jaccards, tps.float(), fps.float(), fns.float()
+    return mes, precisions, recalls, jaccards, tps, fps, fns
