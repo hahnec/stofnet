fix(th): rectify ideal threshold computation

utils/mask2samples.py
@@ -11,18 +11,23 @@ def nms_1d(scores, window_size):
     return suppressed
 
 
-def get_maxima_positions(scores, window_size):
+def get_maxima_positions(scores, window_size, threshold=None):
 
-    suppressed = nms_1d(scores, window_size)
+    suppressed = scores.clone().detach()
+    
+    suppressed = nms_1d(suppressed, window_size)
+
+    if threshold: suppressed[suppressed<threshold] = 0
+
     indices = torch.nonzero(suppressed.squeeze(), as_tuple=False).long()
 
     return indices
 
 
-def samples2nested_list(scores, window_size, upsample_factor=1):
+def samples2nested_list(scores, window_size, threshold=None, upsample_factor=1):
     ''' caution: computationally expensive '''
 
-    indices = get_maxima_positions(scores, window_size)
+    indices = get_maxima_positions(scores, window_size, threshold)
 
     helper_list = []
     nested_list = []
@@ -36,11 +41,9 @@ def samples2nested_list(scores, window_size, upsample_factor=1):
     return nested_list
 
 
-def batch_samples2coords(scores, window_size, threshold=.5, upsample_factor=1):
+def batch_samples2coords(scores, window_size, threshold=None, upsample_factor=1):
 
-    scores[scores<threshold] = 0
-
-    indices = get_maxima_positions(scores, window_size)
+    indices = get_maxima_positions(scores, window_size, threshold)
 
     # catch case where no maxima is found
     if indices.numel() == 0:
@@ -66,10 +69,8 @@ def batch_samples2coords(scores, window_size, threshold=.5, upsample_factor=1):
 
 
 def samples2coords(scores, window_size, threshold=None, upsample_factor=1, echo_max=None):
-
-    if threshold: scores[scores<threshold] = 0
     
-    indices = get_maxima_positions(scores, window_size)
+    indices = get_maxima_positions(scores, window_size, threshold)
 
     # catch case where no maxima is found
     if indices.numel() == 0:

utils/threshold.py
@@ -2,17 +2,18 @@ import torch
 import numpy as np
 from sklearn.metrics import roc_curve
 
+from utils.mask2samples import nms_1d
 
-def find_threshold(masks_pred, masks_true, window_size=None):
 
-    if window_size:
-        window_size = window_size//2*2+1
-        max_scores = torch.nn.functional.max_pool1d(masks_pred, window_size, stride=1, padding=(window_size - 1) // 2).squeeze(0)
-        suppressed = (masks_pred == max_scores).float() * masks_pred
-    else:
-        suppressed = masks_pred
+def find_threshold(masks_pred, masks_true):
 
-    fpr, tpr, thresholds = roc_curve(masks_true.float().numpy().flatten(), suppressed.flatten().float().numpy())
+    # normalize prediction
+    masks_norm = masks_pred.detach().clone()
+    masks_norm -= masks_norm.min()
+    masks_norm /= masks_norm.max()
+    
+    # compute ROC curve results
+    fpr, tpr, thresholds = roc_curve(masks_true.float().numpy().flatten(), masks_norm.flatten().float().numpy())
 
     # calculate the g-mean for each threshold
     gmeans = (tpr * (1-fpr))**.5