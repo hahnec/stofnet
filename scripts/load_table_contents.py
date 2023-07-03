728a8c2e49b673dfc596120115a32520dfee7213
hahnec <christopher.hahne@unibe.ch>
2023-07-03 16:05:48 +0200

    refactor(TeX): adjust TeX commands

--- scripts/load_table_contents.py
+++ scripts/load_table_contents.py
@@ -9,7 +9,7 @@ sorted_runs = sorted(runs, key=lambda run: int(run.name.split('-')[-1]) if run.s
 num_recent_runs = 4
 recent_runs = sorted_runs[:num_recent_runs]
 
-ndigits = 4
+ndigits = 3
 metric_runs = []
 for metric_run in recent_runs:
     model_name = metric_run.summary['model_name'] if 'model_name' in metric_run.summary.keys() else None
@@ -21,26 +21,38 @@ for metric_run in recent_runs:
     # replace None with '-'
 
     model_name, total_dist_mean, total_dist_std, total_time, total_params, total_jaccard = [round(el, ndigits) if isinstance(el, (float, int)) else el for el in [model_name, total_dist_mean, total_dist_std, total_time, total_params, total_jaccard]]
-    row_list = [str(model_name), str(total_params), str(total_dist_mean)+' \pm '+str(total_dist_std), str(total_jaccard), str(total_time)]
+    row_list = [str(model_name), str(total_params), str(total_dist_mean)+' $\pm$ '+str(total_dist_std), str(total_jaccard), str(total_time)]
     metric_runs.append(row_list)
 
 with open("metrics_table.tex", "w") as f:
     # Write the LaTeX table header
-    f.write("\\begin{tabular}{lcccc}\n")
-    f.write("\\hline\n")
-    f.write("Model & Parameters [#] & Distance [Sample] & Jaccard [%] & Time [ms] \\\\\n")
-    f.write("\\hline\n")
+    f.write("\\begin{tabularx}{\linewidth}{ \n \
+        |>{\centering\\arraybackslash}p{5em} \n \
+        |>{\centering\\arraybackslash}p{3em} \n \
+        |>{\centering\\arraybackslash}p{6em} \n \
+        |>{\centering\\arraybackslash}p{3em} \n \
+        |>{\centering\\arraybackslash}p{3em} \n \
+        |}\n")
+    f.write("\\cline{1-5}\n")
+    f.write("Model & Weights [\#] & RMSE [Sample] & Jaccard [\%] & Time [ms] \\\\\n")
+    f.write("\\cline{1-5}\n")
 
     # Iterate over the runs and extract metrics
-    for row_list in metric_runs:
+    for k, row_list in enumerate(metric_runs):
+        
+        # replace model entry
+        row_list[0] = ['Gradient \cite{Hahne:22}', 'Zonzini \cite{Zonzini:2022}', 'SincNet \cite{ravanelli2018speaker}', 'Ours'][k]
+
+        # replace None entries
+        row_list = [col.replace('None', 'n.a.') for col in row_list]
 
         # Write a row for each metric in the LaTeX table
         f.write(" & ".join(str(metric) for metric in row_list))
         f.write(" \\\\\n")
 
     # Write the LaTeX table footer
-    f.write("\\hline\n")
-    f.write("\\end{tabular}")
+    f.write("\\cline{1-5}\n")
+    f.write("\\end{tabularx}")
 
 artifact = None
 if artifact:
