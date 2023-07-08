bdd0998b624f9b28606e2583074d005bbde37c94
hahnec <christopher.hahne@unibe.ch>
2023-07-08 14:10:05 +0200

    refactor(stof): improve input variable interface and handling

--- models/stofnet.py
+++ models/stofnet.py
@@ -5,28 +5,30 @@ import torch.nn.functional as F
 
 from utils.hilbert import hilbert_transform, HilbertTransform
 from utils.sample_shuffle import SampleShuffle1D
-from models.sincnet import SincConv_fast
 
 
 class StofNet(nn.Module):
 
-    def __init__(self, fs, upsample_factor, hilbert_opt=True, concat_oscil=True):
+    def __init__(self, upsample_factor, feat_channels=64, fs=None, hilbert_opt=True, concat_oscil=True):
         super(StofNet, self).__init__()
     
         # input signal handling
+        if fs is not None:
+            from models.sincnet import SincConv_fast
+            self.sinc_filter = SincConv_fast(1, kernel_size=129, sample_rate=fs, in_channels=1, padding=64)
         self.hilbert = HilbertTransform(concat_oscil=concat_oscil) if hilbert_opt else None
         in_channels = 2 if hilbert_opt and concat_oscil else 1
 
         # init first and last layer
-        self.conv1 = nn.Conv1d(in_channels, 64, 9, 1, 4)
-        self.conv13 = nn.Conv1d(64, upsample_factor, 3, 1, 1)
+        self.conv1 = nn.Conv1d(in_channels, feat_channels, 9, 1, 4)
+        self.conv13 = nn.Conv1d(feat_channels, upsample_factor, 3, 1, 1)
 
-        self.sinc_filter = SincConv_fast(1, kernel_size=129, sample_rate=fs, in_channels=1, padding=64)
-        self.semi_global_block = SemiGlobalBlock(64, 64, 80)
+        # init semi-global block
+        self.semi_global_block = SemiGlobalBlock(feat_channels, feat_channels, 80)
 
         # init remaining layers
         for i in range(2, 13):
-            setattr(self, f'conv{i}', nn.Conv1d(64, 64, 7, 1, padding='same'))
+            setattr(self, f'conv{i}', nn.Conv1d(feat_channels, feat_channels, 7, 1, padding='same'))
 
         # shuffle feature channels to high resolution output
         self.sample_shuffle = SampleShuffle1D(upsample_factor)
@@ -37,7 +39,7 @@ class StofNet(nn.Module):
     def forward(self, x):
         
         # input signal handling
-        x = self.sinc_filter(x)
+        x = self.sinc_filter(x) if self.sinc_filter else x
         x = self.hilbert(x) if self.hilbert is not None else x
         
         # first layer
@@ -80,7 +82,7 @@ class SemiGlobalBlock(nn.Module):
         super(SemiGlobalBlock, self).__init__()
 
         self.sample_scale = sample_scale
-        self.feat_scale = sample_scale//10
+        self.feat_scale = max(1, sample_scale//10)
         
         # Contracting path
         self.contract_conv = nn.Conv1d(in_channels, self.feat_scale*out_channels, kernel_size=5, stride=1, padding=1)
