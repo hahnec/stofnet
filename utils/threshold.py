aa44868093a3b117b5286a3ae2b44d04b5634f9d
hahnec <christopher.hahne@unibe.ch>
2023-07-10 19:30:55 +0200

    fix(th): disable normalization in threshold estimation

--- utils/threshold.py
+++ utils/threshold.py
@@ -5,13 +5,16 @@ from sklearn.metrics import roc_curve
 from utils.mask2samples import nms_1d
 
 
-def find_threshold(masks_pred, masks_true):
+def find_threshold(masks_pred, masks_true, norm_opt=False):
 
     # normalize prediction
-    masks_norm = masks_pred.detach().clone()
-    masks_norm -= masks_norm.min()
-    masks_norm /= masks_norm.max()
-    
+    if norm_opt:
+        masks_norm = masks_pred.detach().clone()
+        masks_norm -= masks_norm.min()
+        masks_norm /= masks_norm.max()
+    else:
+        masks_norm = masks_pred
+
     # compute ROC curve results
     fpr, tpr, thresholds = roc_curve(masks_true.float().numpy().flatten(), masks_norm.flatten().float().numpy())
 
