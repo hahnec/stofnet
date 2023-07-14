ea861fe4937c956abbe2c951ce3ecdfc8a9acd7e
hahnec <christopher.hahne@unibe.ch>
2023-07-14 12:41:51 +0200

    feat(train): enable array-based training for chirp dataset

--- bash_scripts/train_chirp_2080.sh
+++ bash_scripts/train_chirp_2080.sh
@@ -11,6 +11,7 @@
 #SBATCH --account=ws_00000
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:rtx2080ti:1
+#SBATCH --array=1-6%1
 
 module load Python/3.8.6-GCCcore-10.2.0
 #module load CUDA/11.3.0-GCC-10.2.0
@@ -28,6 +29,10 @@ python3 -m pip install git+https://github.com/CyberZHG/torch-same-pad.git
 
 python -c "import torch; print(torch.cuda.is_available())"
 
+param_store=~/23_culminate/stofnet/bash_scripts/array_chirp_params.txt
+model=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $1}')
+echo "Model: ${model}"
+
 cd ..
 
-python ./stofnet/main.py evaluate=False logging=train data_dir=./datasets/stof_chirp101_dataset th=Null rf_scale_factor=10
+python ./stofnet/main.py evaluate=False logging=train model=${model} data_dir=./datasets/stof_chirp101_dataset th=Null rf_scale_factor=10 
