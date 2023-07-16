e44289d8302d936ef66b307fb9e8ecf14d85356c
hahnec <christopher.hahne@unibe.ch>
2023-07-16 22:00:26 +0200

    refactor(lib): remove redundant torchaudio dependency

--- utils/transforms.py
+++ utils/transforms.py
@@ -1,37 +1,7 @@
 from torch import Tensor, rand, randn, clamp
 from torch.nn import Module
-from torchaudio import functional as F
-from torchaudio.transforms import Vol
 from numpy import ndarray, random, linspace, pad
 from scipy.interpolate import interp1d
-
-
-class RandomVol(Vol):
-    def __init__(self, 
-        gain: float = 1., 
-        gain_type: str = 'amplitude',
-        normal_distribution: bool = False, 
-    ):
-        super(RandomVol, self).__init__(gain, gain_type)
-
-        self.normal_distribution = normal_distribution
-
-    def forward(self, waveform: (ndarray, Tensor), *args, **kwargs) -> Tensor:
-        
-        if isinstance(waveform, ndarray): waveform = Tensor(waveform)
-
-        rand_num = clamp(randn(1), 0, 1)+.5 if self.normal_distribution else rand(1)
-
-        if self.gain_type == "amplitude":
-            waveform = waveform * self.gain * rand_num
-
-        if self.gain_type == "db":
-            waveform = F.gain(waveform, self.gain * rand_num)
-
-        if self.gain_type == "power":
-            waveform = F.gain(waveform, 10 * math.log10(self.gain * rand_num))
-        
-        return waveform, *args, *kwargs #clamp(waveform, -1, 1)
 
 
 class NormalizeVol(Module):
