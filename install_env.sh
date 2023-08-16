681344dca4bf5135e3532b0c4c92ad7a1bfef699
Hahne Christopher (ch21z139) <ch21z139@submit01.ubelix.unibe.ch>
2023-08-16 22:16:16 +0200

    fix(req): more generic commands for dependencies

--- install_env.sh
+++ install_env.sh
@@ -3,5 +3,6 @@ python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install -r requirements.txt
 
-# obtain repo dependency
-git clone --recurse-submodules https://github.com/hahnec/pala_dataset datasets/pala_dataset
+# obtain repo dependencies
+git clone --recurse-submodules git@github.com:hahnec/pala_dataset datasets/pala_dataset
+python3 -m pip install -r datasets/pala_dataset/requirements.txt
